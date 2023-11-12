0165a902e0a9175b58df55f230b3b74af3e44b1d
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-12 07:21:57 -0700

    additional style changes and added second button with action

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-const textInput = document.getElementById('textInput');
-    textInput.addEventListener('onkeydown', (event) => {
-      if (event.key === 'Enter') {
-        alert('Enter key pressed!');
-      }
-      else if (event.key === "arrowleft") {
-        alert('Left arrow key pressed!')
-      }
-      else if (event.key === 69) {
-        alert("You pressed e!")
-      }
-    })
+const input = document.getElementById('interact');
+
+function clickAction() {
+    alert("Hello!");
+}
+
+input.addEventListener('click', clickAction);
