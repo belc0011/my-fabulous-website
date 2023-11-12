12276f05f9de773fda096cf409032293aa85a977
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-12 07:59:08 -0700

    Discovered unsaved changes in .js file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My Fabulous Website
+This is my personal website where I will document my Software Engineering journey.
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-const input = document.getElementById('interact');
+const input1 = document.getElementById('interact');
 
 function clickAction() {
     alert("Hello!");
 }
 
-input.addEventListener('click', clickAction);
+input1.addEventListener('click', clickAction);
