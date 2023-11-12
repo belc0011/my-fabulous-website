de7a19ede63633b5ac2a4f4962946f99888b5eaa
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-11 21:24:48 -0700

    add clickable button with action

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-
-addEventListener("click", myFunction) {
-    
-}
+const textInput = document.getElementById('textInput');
+    textInput.addEventListener('onkeydown', (event) => {
+      if (event.key === 'Enter') {
+        alert('Enter key pressed!');
+      }
+      else if (event.key === "arrowleft") {
+        alert('Left arrow key pressed!')
+      }
+      else if (event.key === 69) {
+        alert("You pressed e!")
+      }
+    })
